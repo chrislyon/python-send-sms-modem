e4494728ecb64a643e90e8b6c75320e2235ea90c
chris <chris.lyon.tech@gmail.com>
2013-08-06 14:57:36 +0200

    Version 0.1 cela peut fonctionner mais ya pas encore de procedure d'installation ni de documentation

--- send_smsd.py
+++ send_smsd.py
@@ -12,28 +12,105 @@
 ## qui est chargé de lire une ligne
 ## et d'envoyer le fifo sur le modem
 
+## TODO
+## c'est du brut de decoffrage, mais cela marchouille
+## reste a affiner le tout et peut etre ameliorer
+## certaine parties du code qui n'est pas au top
+
 import os, sys, traceback
 import errno
 from stat import *
-#import serial
 import time
 import datetime
 from ConfigParser import SafeConfigParser
-import random
 
 import logging
 import pdb
 
-RANDOM = [True, False, False]
-
-
 ## En exploitation
 CONF_FILE = "/etc/send_smsd.conf"
 ## EN test
-CONF_FILE = "./send_smsd.conf"
+#CONF_FILE = "./send_smsd.conf"
 
 ## Nombre maximum d'essai
 MAX_TRY = 5
+
+## Variable de status de retour 
+SMS_OK = True
+SMS_KO = False
+
+## -------------------
+## ENHANCED SERIAL
+## -------------------
+
+"""Enhanced Serial Port class
+part of pyserial (http://pyserial.sf.net)  (C)2002 cliechti@gmx.net
+
+another implementation of the readline and readlines method.
+this one should be more efficient because a bunch of characters are read
+on each access, but the drawback is that a timeout must be specified to
+make it work (enforced by the class __init__).
+
+this class could be enhanced with a read_until() method and more
+like found in the telnetlib.
+"""
+
+from serial import Serial
+
+class EnhancedSerial(Serial):
+    def __init__(self, *args, **kwargs):
+        #ensure that a reasonable timeout is set
+        timeout = kwargs.get('timeout',0.1)
+        if timeout < 0.01: timeout = 0.1
+        kwargs['timeout'] = timeout
+        Serial.__init__(self, *args, **kwargs)
+        self.buf = ''
+        
+    def readline(self, maxsize=None, timeout=1):
+        """maxsize is ignored, timeout in seconds is the max time that is way for a complete line"""
+        tries = 0
+        while 1:
+            self.buf += self.read(512)
+            pos = self.buf.find('\n')
+            if pos >= 0:
+                line, self.buf = self.buf[:pos+1], self.buf[pos+1:]
+                return line
+            tries += 1
+            if tries * self.timeout > timeout:
+                break
+        line, self.buf = self.buf, ''
+        return line
+
+    def readlines(self, sizehint=None, timeout=1):
+        """read all lines that are available. abort after timout
+        when no more data arrives."""
+        lines = []
+        while 1:
+            line = self.readline(timeout=timeout)
+            if line:
+                lines.append(line)
+            if not line or line[-1:] != '\n':
+                break
+        return lines
+
+## --------
+## MODEM
+## --------
+## On surclasse la classe deja surclasse
+## vive l'objet :)
+class Modem(EnhancedSerial):
+    ## On se simplifie la vie
+    def write(self, texte):
+        #print "Modem >> %s " % texte
+        texte += '\r\n'
+        super(Modem, self).write(texte)
+
+    ##On enleve les \r\n inutiles
+    def readlines(self, timeout):
+        r = super(Modem, self).readlines(timeout)
+        r = [ l.rstrip() for l in r ]
+        return r
+
 
 ## -------------------
 ## Sortie sur erreur
@@ -51,46 +128,106 @@ def log(msg):
     #ts=datetime.datetime.now()
     #print "%s : %s" % (ts.strftime('%j %X'), msg)
 
+
 ## -----------------------------
 ## Envoi d'un sms sur le modem
 ## -----------------------------
 def send_sms(no, msg):
-    log("no=[%s] msg=[%s]" % (no, msg) )
-    time.sleep(1)
-    ## Pour l'instant rien ne fonctionne :)
-    return random.choice( RANDOM )
-### Voici la suite pour envoyer sur le modem
-#    ser = serial.Serial(
-#        port='/dev/ttyACM0',
-#        baudrate=115200,
-#        parity=serial.PARITY_NONE,
-#        stopbits=serial.STOPBITS_ONE,
-#        bytesize=serial.EIGHTBITS
-#    )
-#    ser.open()
-#    ser.isOpen()
-#    ## Envoi de AT+CMGF=1
-#    ser.write("AT+CMGF=1\r\n")
-#    time.sleep(1)
-#    out=''
-#    while ser.inWaiting() > 0:
-#        out += ser.read()
-#    #print "out=%s" % out
-#    ## Envoi de AT+CMGS
-#    ser.write("AT+CMGS=%s\r\n" % no)
-#    time.sleep(1)
-#    out=''
-#    while ser.inWaiting() > 0:
-#        out += ser.read()
-#    #print "out=%s" % out
-#    ## Envoi du message + CTRLZ
-#    ser.write("%s %s" % (msg, chr(26)))
-#    time.sleep(1)
-#    out=''
-#    while ser.inWaiting() > 0:
-#        out += ser.read()
-#    #print "out=%s" % out
-#    ser.close()
+    ## Variable de retour
+    STATUS_SMS = SMS_KO
+    MESSAGE = "ERREUR : Message non initialise"
+
+    ## Ouverture du port
+    port = '/dev/ttyACM0'
+    try:
+        ser = Modem(port)
+    except:
+        return SMS_KO,  "Probleme ouverture port : %s " % port
+
+    ## Synchronisation
+    log( "Synchro Modem" )
+    n = 0
+    while True:
+        ser.write('AT')
+        r = ser.readlines(1)
+        log( "   r = %s " % r )
+        n += 1
+        if r[1] == 'OK' or n > 5:
+            break
+    ##
+    ser.write('ATI')
+    r = ser.readlines(1)
+    log( "   r = %s " % r )
+    ## Note la premiere ligne est l'echo de la commande
+    OK  = r[1] == 'Cinterion'
+    OK *= r[2] == 'EGS3'
+    OK *= r[3][0:8] == 'REVISION'
+    OK *= r[5] == 'OK'
+    if OK:
+        log( "Synchro OK" )
+    else:
+        return SMS_KO, "Probleme synchro MODEM : %s " % r
+
+    ## Envoi de AT+CMGF=1 (passage en mode texte)
+    log("Passage en mode texte" )
+    ser.write('AT+CMGF=1')
+    r = ser.readlines(1)
+    log( "   r = %s " % r )
+    OK  = r[1] == 'OK'
+    if OK:
+        log("Mode texte OK")
+    else:
+        return SMS_KO, "Probleme MODEM passage en mode Texte"
+
+    ## Envoi de AT+CMGS=No_Tel
+    log( "Envoi de No de TEL" )
+    ser.write('AT+CMGS=%s' % no)
+    ## Ici il faut attendre '>'
+    r = ser.readlines(1)
+    log( "    r = %s " % r )
+    OK  = r[1] == '>'
+    if OK:
+        log( "TEL Ok envoi du msg" )
+        ser.write('%s %s' % (msg, chr(26)))
+        ## Lecture de l'echo du texte
+        r = ser.readlines(1)
+        log( "    r = %s " % r )
+        ## Attente 
+        time.sleep(2)
+        ## Lecture du status
+        ## OK => r[3]
+        ## ERROR => r[1]
+        r = ser.readlines(1)
+        if len(r) < 1:
+            ## ON attend un peu et on recommence
+            time.sleep(2)
+            r = ser.readlines(1)
+            if len(r) < 1:
+                ## ON attend un peu et on recommence
+                time.sleep(2)
+                r = ser.readlines(1)
+                if len(r) < 1:
+                    return SMS_KO, "PAS DE STATUS ENVOI"
+        ## on continue
+        log( "    r = %s " % r )
+        ERROR = r[1] == 'ERROR'
+        if len(r) >= 3:
+            OK = r[3] == 'OK'
+        else:
+            OK = False
+        if OK:
+            log( "Message bien envoye" )
+            STATUS_SMS = SMS_OK
+            MESSAGE = "Message envoye"
+        elif ERROR:
+            return SMS_KO, "ERREUR sur envoi du message %s " % r
+        else:
+            return SMS_KO, "ERREUR : mauvais status pb modem ? %s " % r
+    else:
+        return SMS_KO, "Probleme envoi NO TEL"
+    log( "Fermeture Modem" )
+    ser.close()
+    return STATUS_SMS, MESSAGE
 
 ## ----------------------------------------------
 ## Si le fichier n'existe pas
@@ -150,9 +287,11 @@ def loop(fifo):
     log("Open Fifo")
     io = os.open(fifo, os.O_RDONLY | os.O_NONBLOCK)
     while not EXIT:
-        log("Entering for loop")
+        ## Trop verbeux
+        #log("Entering for loop")
         for l in read_line(io):
-            log( "recv : [%s] " % l )
+            if l:
+                log( "recv : [%s] " % l )
             if l in ('STOP', 'SHUTDOWN'):
                 log('receive stop')
                 EXIT=True
@@ -162,23 +301,26 @@ def loop(fifo):
             msg = ''.join(all)
             if no and msg:
                 log("Sending sms")
-                if send_sms(no, msg):
+                ST, MSG = send_sms(no, msg)
+                if ST:
                     log("sms sended")
                 else:
                     n = NB_TRY.get(no, 0)
                     NB_TRY[no] = n+1
                     EN_ATTENTE.append( (no, msg) )
-                    log("sms not sended no=%s NB_TRY=%s" % (no, NB_TRY[no]))
+                    log("sms not sended no=%s NB_TRY=%s MSG=%s" % (no, NB_TRY[no], MSG))
 
         else:
-            log("En attente ? %s " % len(EN_ATTENTE))
+            ## Trop verbeux
+            #log("En attente ? %s " % len(EN_ATTENTE))
             while len(EN_ATTENTE):
                 sms = EN_ATTENTE.pop(0)
                 log("after pop : En attente ? %s " % len(EN_ATTENTE))
                 no = sms[0]
                 msg = sms[1]
                 if NB_TRY[no] < MAX_TRY:
-                    if send_sms(no, msg):
+                    ST, MSG = send_sms(no, msg)
+                    if ST:
                         log("sms %s sended after %s tries" % (no, NB_TRY[no]))
                     else:
                         ## on remet dans la boucle
@@ -186,13 +328,14 @@ def loop(fifo):
                         ## On incremente le compteur
                         n = NB_TRY.get(no, 0)
                         NB_TRY[no] = n+1
-                        log("sms not sended no=%s NB_TRY=%s" % (no, NB_TRY[no]))
+                        log("sms not sended no=%s NB_TRY=%s MSG=%S" % (no, NB_TRY[no], MSG))
                 else:
                     log("Abandon pour %s apres %s essais" % (no, NB_TRY[no]))
                     ## c'est deja retirer de EN_ATTENTE
                     ## reste NB_TRY
                     del NB_TRY[no]
-        log("Attente")
+        ## Trop verbeux
+        #log("Attente")
         time.sleep(3)
         ## J'ai tout lu mais ais je bien bien tout envoye
         ## On repart :)
